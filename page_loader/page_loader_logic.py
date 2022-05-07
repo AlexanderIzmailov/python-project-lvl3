ceb137c670774bf32dd5a396ea5b2bba2e238494
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-05-07 19:54:22 +0300

    Add page-loader

--- page_loader/page_loader_logic.py
+++ page_loader/page_loader_logic.py
@@ -1,2 +1,26 @@
-def page_loader():
-    print("AAA")
+import re
+import os
+import requests
+
+
+def change_name(name):
+    url_parse = re.search(
+        "(?<=\/\/).*(?=\.\w*$)|(?<=\/\/).*",        # noqa: W605
+        name)
+    result = re.sub("\W", "-", url_parse[0])        # noqa: W605
+
+    return result + ".html"
+
+
+def page_loader(url, path):
+    r = requests.get(url)
+    file_name = change_name(url)
+
+    output_file = os.path.join(path, file_name)
+
+    with open(output_file, "w") as f:
+        f.write(r.text)
+
+    print(output_file)
+
+    return output_file
--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,8 +1,28 @@
 from page_loader import page_loader
+import os
+import argparse
+
+parser = argparse.ArgumentParser(
+    description='some description',
+    usage="page-loader [options] <url>")
+parser.add_argument("url", help=argparse.SUPPRESS)
+parser.add_argument(
+    "-V", "--version",
+    help='output the version number',
+    action='version')
+parser.add_argument(
+    "-o", "--output",
+    help='output dir (default: "/app")',
+    metavar="",
+    default=os.getcwd())
+args = vars(parser.parse_args())
+
+url = args["url"]
+output_path = args["output"]
 
 
 def main():
-    page_loader()
+    page_loader(url, output_path)
 
 
 if __name__ == '__main__':
