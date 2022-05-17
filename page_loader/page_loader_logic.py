d60610fa035f33cf073f6c6cf95a3e86bffd6062
AlexanderIzmailov <a.a.izmailov@gmail.com>
2022-05-17 21:18:40 +0300

    Change names of variables for adding another files

--- page_loader/page_loader_logic.py
+++ page_loader/page_loader_logic.py
@@ -22,37 +22,37 @@ def change_name(url):
     return result
 
 
-def take_img_urls(url, domain, img_dir_name, img_dir_path):
-    url_parse = urlparse(url)
-    netloc = url_parse.netloc
+def take_files_urls(file_url, domain, files_dir_name, files_dir_path):
+    file_url_parse = urlparse(file_url)
+    netloc = file_url_parse.netloc
 
     if netloc == "":
         domain_parse = urlparse(domain)
         netloc = domain_parse.netloc
-        url = urljoin(domain, url)
+        file_url = urljoin(domain, file_url)
 
-    path = url_parse.path
-    path_clear = re.search(r".*(?=\.|\/$)|^.*", path)    # noqa: W605
-    path_clear = path_clear[0] if path_clear[0][0] == "/" else "/" + path_clear[0]  # noqa: E501
-    path_ext = re.search(r"\.\w+$", path)    # noqa: W605
+    file_path = file_url_parse.path
+    file_path_clear = re.search(r".*(?=\.|\/$)|^.*", file_path)    # noqa: W605
+    file_path_clear = file_path_clear[0] if file_path_clear[0][0] == "/" else "/" + file_path_clear[0]  # noqa: E501
+    file_path_ext = re.search(r"\.\w+$", file_path)    # noqa: W605
 
-    img_file_name = re.sub(r"\W", "-", netloc + path_clear)   # noqa: W605
-    img_url_for_html = os.path.join(img_dir_name, img_file_name + path_ext[0])
+    file_name = re.sub(r"\W", "-", netloc + file_path_clear)   # noqa: W605
+    file_url_for_html = os.path.join(files_dir_name, file_name + file_path_ext[0])  # noqa: E501
 
-    name_with_ext = img_file_name + path_ext[0]
-    full_img_path = os.path.join(img_dir_path, img_dir_name, name_with_ext)
+    name_with_ext = file_name + file_path_ext[0]
+    full_file_path = os.path.join(files_dir_path, files_dir_name, name_with_ext)
 
-    return url, img_url_for_html, full_img_path
+    return file_url, file_url_for_html, full_file_path
 
 
-def download_and_change_url_img(soup_img_object, url, images_dir_name, path):
-    img_url = soup_img_object["src"]
-    url_for_download, url_for_html, path_for_file = take_img_urls(img_url, url, images_dir_name, path)  # noqa: E501
-    img_data = requests.get(url_for_download).content
-    with open(path_for_file, 'wb') as handler:
-        handler.write(img_data)
+def download_and_change_url_file(soup_object, main_url, files_dir_name, path, tag, open_mode):   # noqa: E501
+    file_url = soup_object[tag]
+    url_for_download, url_for_html, path_for_file = take_files_urls(file_url, main_url, files_dir_name, path)  # noqa: E501
+    object_data = requests.get(url_for_download).content
+    with open(path_for_file, open_mode) as handler:
+        handler.write(object_data)
 
-    soup_img_object["src"] = url_for_html
+    soup_object[tag] = url_for_html
 
 
 def page_loader(url, path):
@@ -63,20 +63,20 @@ def page_loader(url, path):
 
     images = soup.find_all('img')
     if len(images) > 0:
-        images_dir_name = change_name(url) + "_files"
+        files_dir_name = change_name(url) + "_files"
 
-        images_dir = os.path.join(path, images_dir_name)
-        os.mkdir(images_dir)
+        files_dir = os.path.join(path, files_dir_name)
+        os.mkdir(files_dir)
 
         create_file_flag = False
         for k in images:
             img_domain = urlparse(k["src"]).netloc
             if domain == img_domain or img_domain == "":
                 create_file_flag = True
-                download_and_change_url_img(k, url, images_dir_name, path)
+                download_and_change_url_file(k, url, files_dir_name, path, "src", "wb")  # noqa: E501
 
         if not create_file_flag:
-            os.rmdir(images_dir)
+            os.rmdir(files_dir)
 
     html_content = soup.prettify()
 
